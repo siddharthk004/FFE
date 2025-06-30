3f05f024307de1eba7186ddb82993f157c06f340
siddharthk004 <siddharthkardile5@gmail.com>
2025-06-30 09:37:10 +0530

    Added FlyFresh as normal folder

---
+++ FlyFresh/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Header/Navbar";
+
+function App() {
+  return (
+    <div className="bg-blue-300 h-screen w-full">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ FlyFresh/src/components/Header/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="flex bg-green-500 h-[10%] ">
+    <div className="bg-zinc-400 h-full w-[20vw]"></div>
+    <div className="text-red-200 h-full w-[20vw] ">e</div>
+  </div>;
+}
+
+export default Navbar;
